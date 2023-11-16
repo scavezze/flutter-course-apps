328a03d800db0ab1bc62da62e1f8ad9738cda434
Nate Scavezze <nate.scavezze@gmail.com>
2023-11-15 17:53:03 -0700

    form validation

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
@@ -14,6 +16,7 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
 
   void _presentDatePicker() async {
     final now = DateTime.now();
@@ -29,6 +32,57 @@ class _NewExpenseState extends State<NewExpense> {
     setState(() {
       _selectedDate = pickedDate;
     });
+  }
+
+  void submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInValid = enteredAmount == null || enteredAmount <= 0;
+
+    var formIsInValid = false;
+    List<String> messages = [];
+
+    if (_titleController.text.trim().isEmpty) {
+      formIsInValid = true;
+      messages.add("Title is required.");
+    }
+
+    if (amountIsInValid) {
+      formIsInValid = true;
+      messages.add("Amount is invalid.");
+    }
+
+    if (_selectedDate == null) {
+      formIsInValid = true;
+      messages.add("Date is required");
+    }
+
+    if (formIsInValid) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text('Invalid input'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              },
+              child: Text('Okay'),
+            )
+          ],
+          content: IntrinsicHeight(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('The following is invalid.'),
+                SizedBox(height: 20),
+                for (var message in messages) Text(message),
+              ],
+            ),
+          ),
+        ),
+      );
+      return;
+    }
   }
 
   @override
@@ -89,8 +143,34 @@ class _NewExpenseState extends State<NewExpense> {
               ),
             ],
           ),
+          const SizedBox(
+            height: 16,
+          ),
           Row(
             children: [
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(
+                          category.name.toUpperCase(),
+                        ),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  if (value == null) {
+                    return;
+                  }
+
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                },
+              ),
+              const Spacer(),
               TextButton(
                 onPressed: () {
                   Navigator.pop(context);
@@ -100,10 +180,7 @@ class _NewExpenseState extends State<NewExpense> {
                 ),
               ),
               ElevatedButton(
-                onPressed: () {
-                  print(_titleController.text);
-                  print(_amountController.text);
-                },
+                onPressed: submitExpenseData,
                 child: const Text(
                   'Save Expense',
                 ),
